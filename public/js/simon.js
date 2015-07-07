25f6ed3c36748eb99ec158b60b1395e11aa42a24
dgcollier <david@gcollier.me>
2015-07-07 13:10:55 -0500

    Finished Simon. High Score, Quit, EndGame, and Round counter work. Go to this version if re-doing user array does not work.

--- public/js/simon.js
+++ public/js/simon.js
@@ -1,7 +1,150 @@
 "use strict";
-(function () {
+    // Declare variables for gamer and Simon's "choices" in the game
+    // Declare variable for jQuery array with all buttons
+    var gamerClick;
+    var simonClick;
 
-    
+    // Function called when gamer clicks "Start Game" or wants to continue after losing
+    // Resets Simon's array for game reset
+    function startGame () {
+        simonClick = [];
+        simonsTurn();
+    };
 
-})();
+    // Function called when new game is started
+    // Resets gamer's array for game reset
+    //
+    function simonsTurn () {
+        gamerClick = [];
+        checkHighScore();
+        selectRandom();
+        replayArray();
+    };
+
+    function selectRandom () {
+        var random = Math.floor(Math.random() * 4);
+        var boxes = $(".box");
+        var highlightButton = boxes[random];
+        var id = highlightButton.id
+        console.log(id);
+        simonClick.push(id);
+        console.log(simonClick);
+    };
+
+    function highlightBox (box) {
+           box.addClass("highlight");
+        setTimeout(function () {
+            box.removeClass("highlight");
+        }, 500);
+    };
+
+    function replayArray () {
+
+        disableGamerClick();
+
+        $("#round").text("Round: " + simonClick.length);
+
+        var i = 0;
+        
+        var intervalId = setInterval(function () {
+            
+            highlightBox($("#" + simonClick[i]));
+            
+            i++;
+
+            if (i >= simonClick.length){
+                clearInterval(intervalId);
+                enableGamerClick();
+            }
+        }, 1000);
+    };
+
+    function compareArrays () {
+        //set variable for comparison
+        var gamerError = false;
+        // loop through gamerClick and compare arrays
+        for (var i = 0; i < gamerClick.length; i++) {
+            if (simonClick[i] != gamerClick[i]) {
+                gamerError = true;
+                break;
+            }
+        }
+
+        if (gamerError) {
+            endGame();
+        } else if (gamerClick.length == simonClick.length) {
+            simonsTurn();
+        }
+    };
+
+    function endGame () {
+        // reload page, prompt to play again
+
+        var loser = confirm("You lost. Try again?");
+        if (loser) {
+            // simonClick = [];
+            // gamerClick = [];
+            startGame();
+        } else {
+            location.reload(true);
+        }
+    };
+
+    function quitGame () {
+        // stop the game
+        if (simonClick.length < 5) {
+            alert("Your final score was " + (simonClick.length - 1) + ". Did you even try, bro?");
+        } else if (simonClick.length < 7) {
+            alert("Your final score was " + (simonClick.length - 1) + ". Good effort.");
+        } 
+        location.reload(true);
+
+    };
+
+    function gamerClicked () {
+        // create variable to collect id of dot clicked
+        var gamerChoice = this.id
+        console.log(gamerChoice);
+        // call highlight on *this* 
+        highlightBox($(this));
+        // add id of dot clicked by user to gamerClick array
+        gamerClick.push(gamerChoice);
+        console.log(gamerClick);
+        // call compareArrays();
+        compareArrays();
+    };
+
+    function enableGamerClick () {
+        $("#0").on("click", gamerClicked);
+        $("#1").on("click", gamerClicked);
+        $("#2").on("click", gamerClicked);
+        $("#3").on("click", gamerClicked);
+    };
+
+    function disableGamerClick () {
+        $("#0").off("click", gamerClicked);
+        $("#1").off("click", gamerClicked);
+        $("#2").off("click", gamerClicked);
+        $("#3").off("click", gamerClicked);
+    };
+
+    function checkHighScore () {
+
+        var currentScore = simonClick.length;
+        var highScore = 0;
+
+        if(currentScore > highScore) {
+            $("#high-score").text("High Score: " + currentScore);
+        } else {
+            $("#high-score").text("High Score: " + highScore);
+        }
+    };
+
+    $("#start").click(function() {
+        startGame();
+    });
+
+    $("#quit").click(function() {
+        quitGame();
+    });
 
